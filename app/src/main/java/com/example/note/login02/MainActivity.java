685cab542a4b1a5da6c960aee6e0bedd442c6a5b
marioBurgosLed <mario.burgos.led@gmail.com>
2017-05-16 10:17:04 -0300

    Initial commit

---
+++ app/src/main/java/com/example/note/login02/MainActivity.java
@@ -0,0 +1,248 @@
+package com.example.note.login02;
+
+
+import com.example.note.login02.R;
+import com.example.note.login02.AppController;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import java.util.HashMap;
+
+public class MainActivity extends Activity {
+
+    // json object response url
+    private String urlJsonObj = "http://sigequip.esy.es/getLogin2.php";
+
+    // json array response url
+    private String urlJsonArry = "http://sigequip.esy.es/getLogin2.php";
+
+    private static String TAG = MainActivity.class.getSimpleName();
+    private Button btnMakeObjectRequest, btnMakeArrayRequest;
+
+    // Progress dialog
+    private ProgressDialog pDialog;
+
+    private TextView txtResponse;
+    private EditText etUsuario, etPass;
+
+    // temporary string to show the parsed response
+    private String jsonResponse;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnMakeObjectRequest = (Button) findViewById(R.id.btnObjRequest);
+        btnMakeArrayRequest = (Button) findViewById(R.id.btnArrayRequest);
+        txtResponse = (TextView) findViewById(R.id.txtResponse);
+        etUsuario=(EditText)findViewById(R.id.etNombre);
+        etPass=(EditText)findViewById(R.id.etPass);
+
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Please wait...");
+        pDialog.setCancelable(false);
+
+        btnMakeObjectRequest.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // making json object request
+                Loguearse();
+            }
+        });
+
+        btnMakeArrayRequest.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // making json array request
+                makeJsonArrayRequest();
+            }
+        });
+
+    }
+
+    /*
+    Metodo para loguearse
+     */
+    private void Loguearse() {
+        showpDialog();
+        JsonArrayRequest req = new JsonArrayRequest(urlJsonArry, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d(TAG, response.toString());
+
+                try {
+                    jsonResponse = "";
+                    for (int i = 0; i < response.length(); i++) {
+
+                        JSONObject person = (JSONObject) response.get(i);
+
+                        String nomb=person.getString("usuNombre");
+                        String contra=person.getString("usuPass");
+
+                        if(contra.equals(etPass.getText().toString())){
+
+                            Toast.makeText(getApplicationContext(),"Bienvenido",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                            startActivity(intent);
+
+                        }else{
+                            Toast.makeText(getApplicationContext(),"verifique su contraseña",Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+
+                    txtResponse.setText(jsonResponse);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+                hidepDialog();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hidepDialog();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(req);
+    }
+
+
+    /* Method to make json array request where response starts with [
+    * */
+    private void makeJsonArrayRequest() {
+        showpDialog();
+        JsonArrayRequest req = new JsonArrayRequest(urlJsonArry, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+
+                        try {
+                            jsonResponse = "";
+                            for (int i = 0; i < response.length(); i++) {
+
+                                JSONObject person = (JSONObject) response.get(i);
+
+                                String usuNombre=person.getString("usuNombre");
+                                String usuPass=person.getString("usuPass");
+
+                                jsonResponse += "usuNombre: " + usuNombre + "\n\n";
+                                jsonResponse += "usuPass: " + usuPass + "\n\n";
+                            }
+
+                            txtResponse.setText(jsonResponse);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(),
+                                    "Error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                        hidepDialog();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hidepDialog();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(req);
+    }
+
+
+    private void showpDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hidepDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+}
+
+
+/**
+ * Method to make json object request where json response starts wtih {
+ * */
+
+/*
+private void makeJsonObjectRequest() {
+
+    showpDialog();
+    final JsonObjectRequest jsonObjReq = new JsonObjectRequest(Method.GET, urlJsonObj,(String)null, new Response.Listener<JSONObject>() {
+
+                @Override
+                public void onResponse(JSONObject response) {
+                    Log.d(TAG, response.toString());
+
+                    try {
+
+                        String usuNombre=response.getString("usuNombre");
+                        String usuPass=response.getString("usuPass");
+
+                        jsonResponse += "url: " + usuNombre + "\n\n";
+                        jsonResponse += "usuPass: " + usuPass + "\n\n";
+                        txtResponse.setText(jsonResponse);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(getApplicationContext(),
+                                "Error: " + e.getMessage(),
+                                Toast.LENGTH_LONG).show();
+                    }
+
+                    hidepDialog();
+                }
+            }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    VolleyLog.d(TAG, "Error: " + error.getMessage());
+                    Toast.makeText(getApplicationContext(),
+                            error.getMessage(), Toast.LENGTH_SHORT).show();
+                    // hide the progress dialog
+                    hidepDialog();
+                }
+            });
+
+    // Adding request to request queue
+    AppController.getInstance().addToRequestQueue(jsonObjReq);
+}
+*/
